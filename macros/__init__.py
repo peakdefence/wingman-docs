22b56b61ffe84b80526f75b9bf52111e9220be07
jurispuce <juris@peakdefence.com>
2025-08-07 09:08:16 +0000

    Deployed 35f94cbb9630f7a640866a475f34d8995adc2bfd

---
+++ macros/__init__.py
@@ -0,0 +1,12 @@
+# WINGMAN Documentation Macros
+# This file initializes the macros package for MKDocs
+
+# You can define global variables or functions here that will be available in your macros
+
+# Example:
+# def define_env(env):
+#     """Define variables and macros for the documentation."""
+#     @env.macro
+#     def version():
+#         """Return the current version of WINGMAN."""
+#         return "1.0.0"
